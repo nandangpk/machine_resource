62da5cc4cd1dbbe4e9c91cc0aa02435e27f3509c
nandangpk <nandangpermanakusuma@gmail.com>
2023-12-24 04:44:06 +0700

    Exception handling for missing Linux package

--- lib/machine_resource.rb
+++ lib/machine_resource.rb
@@ -3,32 +3,40 @@ require "machine_resource/version"
 module MachineResource
 
   def self.cpu
-    cpu_usage = []
-    data = `sar -P ALL 1 1`.split("\n")
-    data.shift; data.shift; data.shift; data.shift
-    data.each do |d|
-      break if d == ""
-      cpu_usage << self.float_2_digit((100 - d.split(" ").last.to_f))
+    begin
+      cpu_usage = []
+      data = `sar -P ALL 1 1`.split("\n")
+      data.shift; data.shift; data.shift; data.shift
+      data.each do |d|
+        break if d == ""
+        cpu_usage << self.float_2_digit((100 - d.split(" ").last.to_f))
+      end
+      cpu_usage
+    rescue Errno::ENOENT => e
+      "'sysstat' package is not installed"
     end
-    cpu_usage
   end
 
   def self.memory(unit = "")
     return "Invalid or missing unit parameter, accept: ['MB', 'GB']" if unit.upcase != 'MB' && unit.upcase != 'GB'
-    memory_usage = nil
-    data = `top -bn1 -1`.split("\n")
-    data.each do |d|
-      next unless d.start_with? "MiB Mem"
-      d = d.split(" ")
-      memory_usage = {
-        total:  self.float_2_digit((unit.upcase == 'MB' ? d[3].to_f : d[3].to_f / 1000)),
-        free:   self.float_2_digit((unit.upcase == 'MB' ? d[5].to_f : d[5].to_f / 1000)),
-        used:   self.float_2_digit((unit.upcase == 'MB' ? d[7].to_f : d[7].to_f / 1000)),
-        cache:  self.float_2_digit((unit.upcase == 'MB' ? d[9].to_f : d[9].to_f / 1000)) 
-      }
-      break;
+    begin
+      memory_usage = nil
+      data = `top -bn1 -1`.split("\n")
+      data.each do |d|
+        next unless d.start_with? "MiB Mem"
+        d = d.split(" ")
+        memory_usage = {
+          total:  self.float_2_digit((unit.upcase == 'MB' ? d[3].to_f : d[3].to_f / 1000)),
+          free:   self.float_2_digit((unit.upcase == 'MB' ? d[5].to_f : d[5].to_f / 1000)),
+          used:   self.float_2_digit((unit.upcase == 'MB' ? d[7].to_f : d[7].to_f / 1000)),
+          cache:  self.float_2_digit((unit.upcase == 'MB' ? d[9].to_f : d[9].to_f / 1000)) 
+        }
+        break;
+      end
+      memory_usage
+    rescue Errno::ENOENT => e
+      "'procps-ng' package is not installed"
     end
-    memory_usage
   end
 
   def self.float_2_digit(number)
